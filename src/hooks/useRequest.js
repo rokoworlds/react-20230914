ff8f2ef7b13433e8a578af0abcc6244e46cf2306
Roman Coropat <roman.koropat@gmail.com>
2023-10-23 18:37:28 +0300

    add: useRequest custom hook

---
+++ src/hooks/useRequest.js
@@ -0,0 +1,23 @@
+import { useDispatch, useSelector } from "react-redux";
+import { selectRequestStatus } from "../redux/ui/request/selectors";
+import { useEffect, useRef } from "react";
+
+export function useRequest(thunk, ...params) {
+    const request = useRef();
+
+    const requestStatus = useSelector((state) => 
+        selectRequestStatus(state, request.current?.requestId));
+    
+    const dispatch = useDispatch();
+    
+    useEffect(() => {
+        request.current = dispatch(thunk(...params));
+
+        return () => {
+            request.current.abort();
+            request.current = null;
+        }
+    }, [...params, thunk]);
+    
+    return requestStatus;
+}
